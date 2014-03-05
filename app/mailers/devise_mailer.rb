cccefc721bb965b655ff932685c6c63efb07ed2d
Ben Saufley <subscriptions@bensaufley.com>
2014-03-05 09:31:15 -0500

    Switching Devise mailer layout

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,6 @@
+class DeviseMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  layout 'email'
+
+end
