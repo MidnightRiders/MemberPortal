9c1d340808a7c03f5260b4271889d227102a09be
Ben Saufley <subscriptions@bensaufley.com>
2014-03-14 13:07:04 -0400

    Cleaning homepage a smidge and eliminating non-participating users from standings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,7 +19,7 @@ class StaticPagesController < ApplicationController
   def standings
     require 'will_paginate/array'
     @users = User.includes(:pick_ems,:rev_guesses).order('username ASC, last_name ASC, first_name ASC')
-    @pick_em_standings = @users.sort_by(&:pick_em_score).reverse.paginate(page: params[:pick_em_p], per_page: 25)
-    @rev_guess_standings = @users.select{|x| !x.rev_guess_score.nil? }.sort_by(&:rev_guess_score).reverse.paginate(page: params[:rev_guess_p], per_page: 25)
+    @pick_em_standings = @users.select{|x| x.pick_em_score > 0 }.sort_by(&:pick_em_score).reverse.paginate(page: params[:pick_em_p], per_page: 25)
+    @rev_guess_standings = @users.select{|x| !x.rev_guess_score.nil? && x.rev_guess_score > 0 }.sort_by(&:rev_guess_score).reverse.paginate(page: params[:rev_guess_p], per_page: 25)
   end
 end
