efed9a6f9f321e7f3f297288a9ce8f3a9ec1a584
Ben Saufley <subscriptions@bensaufley.com>
2014-03-15 23:21:43 -0400

    Fixing strong parameters for user updates etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_next_revs_match
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   before_filter do
     resource = controller_name.singularize.to_sym
@@ -14,6 +15,12 @@ class ApplicationController < ActionController::Base
     flash[:error] = exception.message
     redirect_to root_url
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:last_name, :first_name, :last_name, :address, :city, :state, :postal_code, :phone, :email, :member_since, :username, :password, :password_confirmation, { role_ids: [] })}
+      devise_parameter_sanitizer.for(:account_update){|u| u.permit(:last_name, :first_name, :last_name, :address, :city, :state, :postal_code, :phone, :email, :member_since, :username, :password, :password_confirmation, :current_password, { role_ids: [] })}
+    end
 
   private
     def set_next_revs_match
