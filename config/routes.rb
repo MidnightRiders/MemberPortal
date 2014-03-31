f2c6fd1c6d72ac6d105a2ad229b3af7d5efb0168
Ben Saufley <subscriptions@bensaufley.com>
2014-03-31 16:39:50 -0400

    Re-adding :registerable and just overriding the sign_up route for now

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ MidnightRiders::Application.routes.draw do
 
   resources :clubs
 
+  match 'users/sign_up', to: 'static_pages#home', via: :all
   devise_for :users
   resources :users do
     collection do
