3b1b3d94f312017f4f932d2b336af9f3dd4aa9b3
Ben Saufley <subscriptions@bensaufley.com>
2014-04-22 15:15:05 -0400

    Fixing monthly motm sorting

--- app/controllers/mot_ms_controller.rb
+++ app/controllers/mot_ms_controller.rb
@@ -8,8 +8,9 @@ class MotMsController < ApplicationController
     @months = Match.where('kickoff <= ?', Time.now).group_by{|x| x.kickoff.beginning_of_month }.sort.map(&:first)
     @mstart = params[:date].try(:to_datetime) || Date.today.beginning_of_month
     month_matches = @revs.matches.where(kickoff: (@mstart...@mstart.end_of_month))
+    @match_ids = month_matches.map(&:id)
     @mot_ms   = Player.includes(:motm_firsts,:motm_seconds,:motm_thirds).select{|x| x.mot_m_total > 0 }.sort_by(&:last_name)
-    @mot_m_mo = @mot_ms.select{|x| x.mot_m_total(month_matches.map(&:id)) > 0 }.group_by{|x| x.mot_m_total(month_matches.map(&:id)) }.sort_by(&:first).reverse
+    @mot_m_mo = @mot_ms.select{|x| x.mot_m_total(@match_ids) > 0 }.group_by{|x| x.mot_m_total(@match_ids) }.sort_by(&:first).reverse
     @mot_m_yr = @mot_ms.group_by(&:mot_m_total).sort_by(&:first).reverse
   end
 
