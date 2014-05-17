7beac1221ff2e900538af7b65b28b28fcbf93f16
Ben Saufley <subscriptions@bensaufley.com>
2014-05-17 13:48:42 -0400

    Removing user_roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,13 @@ class Role < ActiveRecord::Base
   has_many :membership_roles, dependent: :destroy
   has_many :memberships, through: :membership_roles
   validates :name, uniqueness: true
+
+  def self.list(verbose=false)
+    rs = all.map{|r| r.name.titleize }
+    if verbose
+      rs.to_sentence
+    else
+      rs.join(', ')
+    end
+  end
 end
--- spec/factories/membership_roles.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :membership_role do
-    membership nil
-    role nil
-  end
-end
