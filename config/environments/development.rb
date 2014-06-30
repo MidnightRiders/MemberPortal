afc265e5ab832b2837e6e762c1b6bc010d1e018b
Ben Saufley <subscriptions@bensaufley.com>
2014-06-29 23:34:31 -0400

    Changing default assets servers to match my usual port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,8 @@ MidnightRiders::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.asset_host = 'http://localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3010' }
+  config.action_mailer.asset_host = 'http://localhost:3010'
   config.action_mailer.logger = Logger.new(STDOUT)
 
   # Do not eager load code on boot.
