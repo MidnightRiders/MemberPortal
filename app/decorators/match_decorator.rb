aad2333934e31dc53c8efafde847c7e269b50bbd
Ben Saufley <bensaufley+git@gmail.com>
2014-07-20 20:02:36 -0400

    Decorators

--- app/decorators/match_decorator.rb
+++ app/decorators/match_decorator.rb
@@ -1,6 +1,20 @@
 class MatchDecorator < Draper::Decorator
   delegate_all
 
+  def rev_guess_button(size='tiny', opponent = false)
+    h.link_to h.rev_guess_path_for(model), class: "button secondary #{size}" do
+      opp = opponent ? (model.home_team == revs ? ' v ' : ' @ ')+  (model.teams - [revs])[0].abbrv : ''
+      h.icon('question fa-fw') + ' RevGuess' + opp + (": #{h.rev_guess_for(model)[1]}" if h.rev_guess_for(model))
+    end
+  end
+
+  def mot_m_button(size='tiny', opponent = false)
+    h.link_to h.mot_m_path_for(model), class: "button secondary #{size}" do
+      opp = opponent ? (model.home_team == revs ? ' v ' : ' @ ') +  (model.teams - [revs])[0].abbrv : ''
+      h.icon('list-ol fa-fw') + ' MotM' + opp + (h.icon('check fa-fw') if h.mot_m_for(model))
+    end
+  end
+
   def pick_em_buttons(*args)
     h.content_tag :div, class: "pick-em-buttons secondary-border border-all #{model.home_team.abbrv.downcase} #{'closed' unless model.kickoff.future?}", title: model.kickoff.future? ? 'Pick ’Em' : 'Voting Closed' do
       h.concat h.content_tag :div, pick_em_button(:home, *args), class: 'home'
@@ -12,27 +26,23 @@ class MatchDecorator < Draper::Decorator
   def pick_em_button(pick, *args)
     opts = args.extract_options!
     user = opts[:user] || h.current_user
-    text = opts[:text]
-    team = model.try("#{pick}_team".to_sym)
-
-    html_classes = ['choice', pick == :draw ? ['secondary'] : ['primary-bg', team.abbrv.downcase]]
-    if model.complete? && model.result == pick
-      html_classes << (user.pick_for(model).try(:correct?) ? 'correct' : 'actual')
-    end
+    team = model.send("#{pick}_team") if pick.in? [:home, :away]
+    html_classes = ['choice', pick == :draw ? ['secondary'] : ['primary-bg', team.abbrv.downcase]].flatten
+    html_classes << (user.pick_for(model).try(:correct?) ? 'correct' : 'actual') if model.complete? && model.result == pick
     if user.pick_result(model) == PickEm::RESULTS[pick]
       html_classes << 'picked'
       html_classes << 'wrong' if user.pick_for(model).try(:wrong?)
     end
 
     content = if team
-      (text || team.crest.blank?) ? team.abbrv : h.image_tag('http://midnightriders.com' + team.crest.url(:thumb), title: team.name)
+      team.crest.blank? ? team.abbrv : h.image_tag('http://midnightriders.com' + team.crest.url(:thumb), title: team.name)
     else
       'D'
     end
 
-    # if model.kickoff.past?
-    #   h.content_tag(:div, content, class: html_classes.join(' '))
-    # else
+    if model.in_past?
+       h.content_tag(:div, content, class: html_classes.join(' '))
+    else
       h.link_to(
         content,
         h.vote_match_pick_ems_path(model),
@@ -49,7 +59,7 @@ class MatchDecorator < Draper::Decorator
         },
         class: html_classes
       )
-    # end
+    end
   end
 
   def pick_em_sub
@@ -67,4 +77,13 @@ class MatchDecorator < Draper::Decorator
       h.concat h.link_to(h.icon('trash-o fa-fw'),  model, method: :delete, data: { :confirm => 'Are you sure?' }, title: 'Destroy') if h.can? :destroy, model
     end
   end
+
+  private
+    def revs
+      Club.find_by(abbrv: 'NE')
+    end
+
+    def opponent
+      (model.home_team == revs ? 'v ' : '@ ')+  (model.teams - [revs])[0].abbrv
+    end
 end
