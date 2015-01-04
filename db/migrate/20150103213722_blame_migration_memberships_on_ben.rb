0c1edcbf628802f9e8ed8c0c5619a9fb08cdc090
Ben Saufley <contact@bensaufley.com>
2015-01-04 01:13:27 -0500

    Starting Relative work

--- db/migrate/20150103213722_blame_migration_memberships_on_ben.rb
+++ db/migrate/20150103213722_blame_migration_memberships_on_ben.rb
@@ -1,10 +1,10 @@
 class BlameMigrationMembershipsOnBen < ActiveRecord::Migration
   def up
     ben = User.find_by(username: 'bensaufley')
-    Membership.where(info: nil).each do |membership|
+    Membership.unscoped.where(info: nil).each do |membership|
       membership.update_attribute(:info, { override: ben.id })
     end
-    Membership.where("info -> 'override' = ?", 'migration').each do |membership|
+    Membership.unscoped.where("info -> 'override' = ?", 'migration').each do |membership|
       membership.info_will_change!
       membership.info['override'] = ben.id
       membership.save validate: false
@@ -12,7 +12,7 @@ class BlameMigrationMembershipsOnBen < ActiveRecord::Migration
   end
 
   def down
-    Membership.where("info -> 'override' = ?", User.find_by(username: 'bensaufley').id.to_s).each do |membership|
+    Membership.unscoped.where("info -> 'override' = ?", User.find_by(username: 'bensaufley').id.to_s).each do |membership|
       membership.info_will_change!
       membership.info['override'] = 'migration'
       membership.save validate: false
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20150104043828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-  enable_extension "pg_stat_statements"
 
   create_table "clubs", force: true do |t|
     t.string   "name"
