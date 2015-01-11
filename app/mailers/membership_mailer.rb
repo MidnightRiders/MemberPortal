26181ca2180e5b3c861444528f10c5e4754ab328
Ben Saufley <contact@bensaufley.com>
2015-01-11 18:51:49 -0500

    Tagging alert emails

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -34,7 +34,7 @@ class MembershipMailer < ActionMailer::Base
   def new_membership_alert(user, membership)
     @user = user
     @membership = membership
-    @title = "New #{@membership.year} #{@membership.type} Membership for #{@user.first_name} #{@user.last_name}"
+    @title = "[MemberPortalAlert] New #{@membership.year} #{@membership.type} Membership for #{@user.first_name} #{@user.last_name}"
     mail(to: 'Membership Chair<membership@midnightriders.com>,President<president@midnightriders.com>,Web Chair<webczar@midnightriders.com>', subject: @title)
   end
 
@@ -42,7 +42,7 @@ class MembershipMailer < ActionMailer::Base
   def membership_cancellation_alert(user, membership)
     @user = user
     @membership = membership
-    @title = "#{@membership.year} #{@membership.type} Membership Refunded for #{@user.first_name} #{@user.last_name}"
+    @title = "[MemberPortalAlert] #{@membership.year} #{@membership.type} Membership Refunded for #{@user.first_name} #{@user.last_name}"
     mail(to: 'Membership Chair<membership@midnightriders.com>,President<president@midnightriders.com>,Web Chair<webczar@midnightriders.com>', subject: @title)
   end
 end
