7e1973fd971ca533d40cdc12049677de297a3e21
Ben Saufley <contact@bensaufley.com>
2015-01-11 18:05:07 -0500

    Forgot to undo this!

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -101,7 +101,7 @@ class Membership < ActiveRecord::Base
         end
         if subscription.to_i == 1
           subscription = customer.subscriptions.create(
-              plan: type,
+              plan: type.downcase,
               trial_end: 1.year.from_now.beginning_of_year.to_i
           )
           self.stripe_subscription_id = subscription.id
