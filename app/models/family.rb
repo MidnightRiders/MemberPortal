0a110e2dcc218a387d221b4ebf109ae2dd714604
Ben Saufley <subscriptions@bensaufley.com>
2015-01-12 16:25:54 -0500

    Fixes for some bugs

--- app/models/family.rb
+++ app/models/family.rb
@@ -6,7 +6,7 @@ class Family < Membership
   private
     def re_up_relatives
       last_membership = user.memberships.where.not(id: id).last
-      if last_membership.present?
+      if last_membership.present? && last_membership.is_a?(Family)
         last_membership.relatives.each do |relative|
           new_relative = relative.clone
           new_relative.year = year
