3339d45b7546ae73dfb16687827f57447c738776
Ben Saufley <contact@bensaufley.com>
2015-02-22 16:06:34 -0500

    Fixing overspecific date selector

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -10,8 +10,8 @@ class MatchesController < ApplicationController
     @matches = Match.unscoped.with_clubs.includes(:pick_ems).where(kickoff: (@start_date..@start_date + 7.days)).order(kickoff: :asc, location: :asc)
     @prev_link = "Previous #{'Game ' if @matches.empty?}Week"
     @next_link = "Next #{'Game ' if @matches.empty?}Week"
-    @prev_date = @matches.empty? ? Match.unscoped.where('kickoff < NOW()').order(kickoff: :asc).last.kickoff.to_date : @start_date - 1.week
-    @next_date = @matches.empty? ? Match.unscoped.where('kickoff > NOW()').order(kickoff: :asc).first.kickoff.to_date : @start_date + 1.week
+    @prev_date = (@matches.empty? ? Match.unscoped.where('kickoff < NOW()').order(kickoff: :asc).last.kickoff : @start_date - 1.week).to_date
+    @next_date = (@matches.empty? ? Match.unscoped.where('kickoff > NOW()').order(kickoff: :asc).first.kickoff : @start_date + 1.week).to_date
   end
 
   # GET /matches/1
