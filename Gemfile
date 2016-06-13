d43beb69edcc7aca3b3ac584b4cbcb86849e8900
Ben Saufley <subscriptions@bensaufley.com>
2016-06-12 22:01:15 -0400

    Update gems, remove former players

--- Gemfile
+++ Gemfile
@@ -63,7 +63,10 @@ group :development, :test do
   gem 'active_record_query_trace'
 end
 
-gem 'database_cleaner', group: :test
+group :test do
+  gem 'database_cleaner'
+  gem 'capybara-webkit'
+end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,41 +8,18 @@
 
 require 'csv'
 
-def create_or_update x, y
-  x.attributes = y
-  new = x.new_record?
-  if new || x.changed?
-    if x.save
-      if new
-        '  Created.'
-      elsif  x.changed.empty?
-        '  No changes.'
-      else
-        '  Updated: ' + x.changed.keys.join(', ')
-      end
-    else
-      ' Could not save:' + x.errors.map{|k,v| "#{k}: #{v}\n"}.join
-    end
-  else
-    '  No changes.'
+print 'Seeding clubs…'
+CSV.foreach(Rails.root.join('lib/assets/clubs.csv'), headers: true, header_converters: :symbol) do |club|
+  binding.pry
+  Club.unscoped.find_or_initialize_by(abbrv: club[:abbrv]).update_attributes(club.to_h)
+end
+puts ' done.'
+
+unless Rails.env.test?
+  print 'Seeding players…'
+  ne_id = Club.find_by(abbrv: 'NE').id
+  CSV.foreach(Rails.root.join('lib/assets/players.csv'), headers: true, header_converters: :symbol) do |player|
+    Player.unscoped.find_or_initialize_by(first_name: player[:first_name], last_name: player[:last_name]).update_attributes(p.to_h.merge({ club_id: ne_id }))
   end
-end
-
-clubs = CSV.parse(File.read(Rails.root.join('lib/assets/clubs.csv')), headers: true)
-clubs.each do |club|
-  club = club.to_hash
-  c = Club.find_or_initialize_by(abbrv: club['abbrv'])
-  new = c.new_record?
-  puts "#{new ? 'Adding' : 'Updating'} #{club['name']}…"
-  puts create_or_update(c, club)
-end
-
-players = CSV.parse(File.read(Rails.root.join('lib/assets/players.csv')), headers: true)
-players.each do |player|
-  player = player.to_hash
-  p = Player.find_or_initialize_by(first_name: player['first_name'], last_name: player['last_name'])
-  new = p.new_record?
-  puts "#{new ? 'Adding' : 'Updating'} #{player['first_name']} #{player['last_name']}…" unless Rails.env.test?
-  p.club_id = Club.find_by(abbrv: 'NE').id
-  puts create_or_update(p, player) unless Rails.env.test?
+  puts ' done.'
 end
