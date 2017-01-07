022c849d0f232780fe4aaa232a1aa279ce719291
Ben Saufley <bensaufley+git@gmail.com>
2017-01-06 22:29:39 -0500

    #8 Strip whitespace from invited Relative's email
    Also treat Membership.info as HashWithIndifferentAccess

--- spec/models/relative_spec.rb
+++ spec/models/relative_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Relative do
-  skip "add some examples to (or delete) #{__FILE__}"
+  it 'strips whitespace from an invited email before validation' do
+    relative = Relative.new(info: { invited_email: ' test.email@with-spaces.com ', pending_approval: true })
+
+    relative.valid?
+
+    expect(relative.info[:invited_email]).to eq('test.email@with-spaces.com')
+  end
 end
