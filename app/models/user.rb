53f56220d6b8f7f0c4571afd3fbf007685ba90ae
Ben Saufley <bensaufley+git@gmail.com>
2017-01-11 23:55:00 -0500

    Remove Roo dependency; begin #12 cleanup of User.import; spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -152,36 +152,34 @@ class User < ActiveRecord::Base
   # TODO: Clean the shit out of this import. Stabilize it.
 
   # User import script. Needs work.
-  def self.import(file, privileges = [], override_id)
+  def self.import(import_users, privileges = [], override_id)
     allowed_attributes = %i(last_name first_name last_name address city state postal_code phone email member_since username)
-    spreadsheet = Roo::Spreadsheet.open(file.path.to_s, extension: 'csv')
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
+    imported_users = []
 
-      next if row['type'] == 'Relative' # TODO: Allow Relative input
+    import_users.each do |import_user|
+      next if import_user[:membership_type] == 'Relative' # TODO: Allow Relative input
 
-      if row['email'].blank?
-        logger.error "No email for #{row['first_name']} #{row['last_name']}"
+      if import_user[:email].blank?
+        logger.error "No email for #{import_user[:first_name]} #{import_user[:last_name]}"
         next
       end
 
-      user = User.where(email: row['email'].strip.downcase).first_or_initialize
+      user = User.where(email: import_user[:email].strip.downcase).first_or_initialize
       if user.new_record?
-        original_uname = row['username'] = "#{row['first_name']}#{row['last_name']}".downcase.gsub(/[^a-z]/, '')
+        original_uname = import_user[:username] = "#{import_user[:first_name]}#{import_user[:last_name]}".downcase.gsub(/[^a-z]/,'')
         i = 0
-        until User.find_by(username: row['username']).nil?
+        until User.find_by(username: import_user[:username]).nil?
           i += 1
-          row['username'] = "#{original_uname}#{i}"
+          import_user[:username] = "#{original_uname}#{i}"
         end
       end
-      user.attributes = row.to_hash.select { |x| allowed_attributes.include? x.to_sym }
+      user.attributes = import_user.select { |x| allowed_attributes.include? x.to_sym  }
       pass = false
       if user.new_record?
-        logger.info "Adding #{row['first_name']} #{row['last_name']}…"
+        logger.info "Adding #{import_user[:first_name]} #{import_user[:last_name]}…"
         user.password = (pass = rand(36**10).to_s(36))
       else
-        logger.info "Updating #{row['first_name']} #{row['last_name']}…"
+        logger.info "Updating #{import_user[:first_name]} #{import_user[:last_name]}…"
         if user.changed?
           logger.info "  Changed: #{user.changes.keys.to_sentence}"
         else
@@ -193,14 +191,17 @@ class User < ActiveRecord::Base
         if m.new_record?
           m.info = { override: override_id }
           m.privileges = privileges
-          m.type = row['membership_type'].titleize || 'Individual'
+          m.type = import_user[:membership_type].titleize || 'Individual'
           logger.info "#{m.year} #{m.type} Membership created for #{user.first_name} #{user.last_name} (#{user.username})" if m.save
         end
+        imported_users << user
         UserMailer.new_user_creation_email(user, pass).deliver if pass
       else
-        logger.error "  Could not save user #{row['first_name']} #{row['last_name']}:\n  " + user.errors.to_hash.map { |k, v| "#{k}: #{v.to_sentence}" }.join("\n  ")
+        logger.error "  Could not save user #{import_user[:first_name]} #{import_user[:last_name]}:\n  " + user.errors.to_hash.map{|k,v| "#{k}: #{v.to_sentence}"}.join("\n  ")
       end
     end
+
+    imported_users
   end
 
   # Outputs CSV
