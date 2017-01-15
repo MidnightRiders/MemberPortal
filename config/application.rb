9b75de892735092324a4a9d4640eb333937eb48d
Ben Saufley <bensaufley+git@gmail.com>
2017-01-15 14:27:37 -0500

    Explicit log_level

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,8 @@ module MidnightRiders
 
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.log_level = ENV['LOG_LEVEL']&.downcase&.to_sym || :debug
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
