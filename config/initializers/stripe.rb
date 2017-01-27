37b08650920592b7c798a5ece21ec642aa689940
Ben Saufley <bensaufley+git@gmail.com>
2017-01-26 23:05:08 -0500

    Make Stripe Public Key Public; move dotenv to dev/test

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,7 @@
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+ENV['STRIPE_PUBLIC_KEY'] = if Rails.env.production?
+  'pk_live_fPWFbD0LtW0JhOuGmtsWQz7l'
+else
+  'pk_test_4oCLBfAnf9nXr0bOjHFLbWI8'
+end
