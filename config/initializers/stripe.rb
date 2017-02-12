8ee1d6e10891cb9fee929bdc1f72b68f7b48c859
Ben Saufley <bensaufley+git@gmail.com>
2017-02-12 00:53:44 -0500

    Add fake Stripe secret key for testing

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
-Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+Stripe.api_key = Rails.env.test? ? 'sk_test_fake4oCLBfAnf9nXr0bOjHFL' : ENV['STRIPE_SECRET_KEY']
 
 ENV['STRIPE_PUBLIC_KEY'] = if Rails.env.production?
   'pk_live_fPWFbD0LtW0JhOuGmtsWQz7l'
