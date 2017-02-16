8aab8c602f66341f87ada404e8545219eb5f1c62
Ben Saufley <bensaufley+git@gmail.com>
2017-02-15 21:53:59 -0500

    Remove unused argument

--- app/services/error_notifier.rb
+++ app/services/error_notifier.rb
@@ -1,6 +1,6 @@
 class ErrorNotifier
   class << self
-    def notify(error, severity: :error, output: nil)
+    def notify(error, severity: :error)
       Rails.logger.public_send(severity, "#{error.class}: #{error.message}")
       Rails.logger.info error.backtrace.to_yaml
       error.message
