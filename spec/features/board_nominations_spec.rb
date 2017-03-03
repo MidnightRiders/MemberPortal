479d883b59889ebff7cba60418663410ed88fb25
Ben Saufley <bensaufley+git@gmail.com>
2017-03-02 23:03:42 -0500

    Extract Board Nominations to be reusasble, keep tests green

--- spec/features/board_nominations_spec.rb
+++ spec/features/board_nominations_spec.rb
@@ -5,11 +5,17 @@ feature 'Board Nominations', type: :feature, js: true do
   let(:positions) {
     %w(At-Large\ Board President Treasurer Membership\ Secretary Web\ Czar Recording\ Secretary Philanthropy\ Chair Merchandise\ Coordinator).sort + ['501(c)(3) Board of Directors']
   }
+
   before(:each) do
+    Timecop.travel(Time.zone.local(Date.current.year, 1, 15))
     login_as user
     visit user_home_path
 
-    click_on 'Submit 2017 Board Nomination'
+    click_on "Submit #{Date.current.year} Board Nomination"
+  end
+
+  after(:each) do
+    Timecop.return
   end
 
   it 'shows the modal' do
