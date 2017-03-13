b8573b9ab65d2cf494779e1482746b5cf223cbb3
Ben Saufley <bensaufley+git@gmail.com>
2017-03-12 21:22:32 -0400

    Some refactoring; match transition animation

--- app/assets/javascripts/components/match_collection.es6.jsx
+++ app/assets/javascripts/components/match_collection.es6.jsx
@@ -1,30 +1,33 @@
 class MatchCollection extends React.Component {
-  render () {
-    if (this.props.matches.length) {
+  matchList() {
+    return this.props.matches.map((match) => {
       return (
-        <ul className="matches">
-          {this.props.matches.map((match) => {
-            return (
-              <Match
-                key={`match-${match.id}`}
-                id={match.id}
-                homeTeam={match.homeTeam}
-                awayTeam={match.awayTeam}
-                homeGoals={match.homeGoals}
-                awayGoals={match.awayGoals}
-                pick={match.pick}
-                kickoff={new Date(match.kickoff)}
-                location={match.location}
-                showAdminUi={this.props.showAdminUi}
-              />
-            );
-          })
-          }
+        <Match
+          key={`match-${match.id}`}
+          id={match.id}
+          homeTeam={match.homeTeam}
+          awayTeam={match.awayTeam}
+          homeGoals={match.homeGoals}
+          awayGoals={match.awayGoals}
+          pick={match.pick}
+          kickoff={new Date(match.kickoff)}
+          location={match.location}
+          showAdminUi={this.props.showAdminUi}
+        />
+      );
+    });
+  }
+
+  render() {
+    if (this.props.matches.length > 0) {
+      return (
+        <ul className={`matches ${this.props.loading ? 'loading' : ''}`}>
+          {this.matchList()}
         </ul>
       );
     } else {
       return (
-        <div className="alert alert-box">
+        <div className={`alert-box no-matches ${this.props.loading ? 'loading' : ''}`}>
           <h2 className="text-center">No matches scheduled this week.</h2>
         </div>
       );
@@ -34,5 +37,6 @@ class MatchCollection extends React.Component {
 
 MatchCollection.propTypes = {
   matches: React.PropTypes.array,
-  showAdminUi: React.PropTypes.bool
+  showAdminUi: React.PropTypes.bool,
+  loading: React.PropTypes.bool
 };
