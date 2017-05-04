5e638f65b1aa71bbce0a09761a00b0455b5048b4
Ben Saufley <bensaufley+git@gmail.com>
2017-05-03 22:29:18 -0400

    Specify whodunnit stuff for PaperTrail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_next_revs_match
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
 
   before_action do
     resource = controller_name.singularize.to_sym
