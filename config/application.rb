4a7b96919cae442be40f1f083dea2e50144111fd
Ben Saufley <bensaufley+git@gmail.com>
2020-01-13 22:06:26 -0500

    Add s3 region

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module MidnightRiders
     config.paperclip_defaults = {
       storage: :s3,
       s3_protocol: :https,
+      s3_region: 'us-east-1',
       s3_credentials: {
         bucket: ENV['S3_BUCKET_NAME'],
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
