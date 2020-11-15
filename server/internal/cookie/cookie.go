290156cc76d24d3e4919f67097c621b910e64968
Ben Saufley <bensaufley+git@gmail.com>
2020-11-14 20:11:59 -0500

    Add tests

--- server/internal/cookie/cookie.go
+++ server/internal/cookie/cookie.go
@@ -13,13 +13,17 @@ var contextKey ctxKey = "cookies"
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookies := []http.Cookie{}
-
-		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), contextKey, &cookies)))
+		next.ServeHTTP(w, r.WithContext(AddToContext(r.Context(), &cookies)))
 
 		for _, ck := range cookies {
 			http.SetCookie(w, &ck)
 		}
 	})
+}
+
+// AddToContext adds a clice of Cookies to a given context
+func AddToContext(ctx context.Context, cookies *[]http.Cookie) context.Context {
+	return context.WithValue(ctx, contextKey, cookies)
 }
 
 // Add takes a context and adds a cookie to it
