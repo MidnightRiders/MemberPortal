2b55f003e1b0e89cdec822dc49ddfe8cca9a7ed6
Ben Saufley <bensaufley+git@gmail.com>
2022-12-02 00:49:27 -0500

    Move to Stripe.js v3

--- app/assets/javascripts/users/memberships.js
+++ app/assets/javascripts/users/memberships.js
@@ -1,16 +1,15 @@
 (function ($) {
-  var stripeParams, subscription, stripePublicKey;
-
-  stripeParams = {
+  var stripeParams = {
     number: '#stripe_card_number',
     exp_month: '#stripe_exp_month',
     exp_year: '#stripe_exp_year',
     cvc: '#stripe_cvc'
   };
 
-  subscription = {
-    stripeInit: function() {
-      window.Stripe.setPublishableKey(stripePublicKey);
+  var subscription = {
+    stripe: undefined,
+    stripeInit: function(publicKey) {
+      this.stripe = Stripe(publicKey);
       subscription.setUpForm();
     },
     setUpForm: function () {
@@ -25,25 +24,27 @@
             value = stripeParams[key];
             card[key] = $(value).val();
           }
-          window.Stripe.createToken(card, subscription.handleStripeResponse);
+          this.stripe.createToken(card).then(subscription.handleStripeResponse);
         }
       });
     },
-    handleStripeResponse: function (status, response) {
-      if (status === 200) {
-        $('#membership_stripe_card_token').val(response.id).closest('form')[0].submit();
-      } else {
+    handleStripeResponse: function (result) {
+      var token = result.token;
+      var error = result.error;
+      if (error) {
         $('#stripe_card_number').closest('form').find(':submit').prop('disabled', false);
         $('.field_with_errors [id^=stripe]').each(function () {
           $(this).closest('.field_with_errors').removeClass('field_with_errors').find('small.error').remove();
         });
-        $(stripeParams[response.error.param]).after('<small class="error">' + response.error.message + '</small>').closest('.row').addClass('field_with_errors');
+        $(stripeParams[response.error.param]).after('<small class="error">' + error.message + '</small>').closest('.row').addClass('field_with_errors');
+      } else {
+        $('#membership_stripe_card_token').val(token).closest('form')[0].submit();
       }
     }
   };
 
   $(function () {
-    stripePublicKey = $('meta[name="stripe-public-key"]').attr('content');
+    var stripePublicKey = $('meta[name="stripe-public-key"]').attr('content');
     $('#show-credit-card-info').on('click', function (e) {
       e.preventDefault();
       $('#credit-card-info').removeClass('hide').hide().slideDown(150).find(':disabled').prop('disabled', false);
@@ -53,7 +54,7 @@
       });
     });
     if (stripePublicKey && $('#credit-card-info').is(':visible')) {
-      subscription.stripeInit();
+      subscription.stripeInit(stripePublicKey);
     }
   });
 
