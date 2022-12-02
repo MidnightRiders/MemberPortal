e495058e499085f73d54cb324010c4e62c85b070
Ben Saufley <bensaufley+git@gmail.com>
2022-12-01 23:21:58 -0500

    Remove references to Stripe tokens from repo
    The checked-in tokens were public and/or test tokens so it shouldn't have been the end of the world but I still just don't like it

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,1 @@
-Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-Stripe.api_key ||= 'sk_test_fake4oCLBfAnf9nXr0bOjHFL' if Rails.env.test?
-
-ENV['STRIPE_PUBLIC_KEY'] = if Rails.env.production?
-  'pk_live_fPWFbD0LtW0JhOuGmtsWQz7l'
-else
-  'pk_test_4oCLBfAnf9nXr0bOjHFLbWI8'
-end
+Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY')
