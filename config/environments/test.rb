99a4d202d6003ffcda31b1ea681830962ad7f8b7
Ben Saufley <bensaufley+git@gmail.com>
2022-12-04 19:07:13 -0500

    debugging env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 MidnightRiders::Application.configure do
+  print("Test environment: #{ENV.inspect}")
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { host: 'localhost:3010' }
