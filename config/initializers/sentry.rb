7c1b438dc415eab57efc7889821d7fae2afbef78
Ben Saufley <bensaufley+git@gmail.com>
2022-12-18 22:40:36 -0500

    Don't run Sentry on asset precompilation

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,6 @@
-if Rails.env.production?
+puts Rake.application.top_level_tasks
+
+if Rails.env.production? && !Rake.application.top_level_tasks.all? { /^assets:/i =~ _1 }
   Sentry.init do |config|
     config.dsn = 'https://bc9541d68fb243c89e2d97f20e2148fe@o4504330520100864.ingest.sentry.io/4504330521870336'
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
