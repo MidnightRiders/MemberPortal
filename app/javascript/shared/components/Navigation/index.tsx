c59af18ef508704c6e2a8a4c4ba31f38f2aa6c1f
Ben Saufley <bensaufley+git@gmail.com>
2023-01-14 21:06:13 -0500

    Collapse nav on mobile

--- app/javascript/shared/components/Navigation/index.tsx
+++ app/javascript/shared/components/Navigation/index.tsx
@@ -1,3 +1,7 @@
+import clsx from 'clsx';
+import type { JSX } from 'preact';
+import { useCallback, useState } from 'preact/hooks';
+
 import NavNode from '~shared/components/Navigation/NavNode';
 import useNav from '~shared/components/Navigation/useNav';
 
@@ -5,10 +9,25 @@ import styles from './styles.module.css';
 
 const Navigation = () => {
   const navItems = useNav();
+  const [mobileExpand, setMobileExpand] = useState(false);
+
+  const toggleMobileExpand = useCallback<
+    JSX.GenericEventHandler<HTMLButtonElement>
+  >((e) => {
+    e.preventDefault();
+    setMobileExpand((v) => !v);
+  }, []);
 
   return (
     <nav class={styles.nav}>
-      <ul>
+      <button
+        type="button"
+        class={styles.mobileExpand}
+        onClick={toggleMobileExpand}
+      >
+        <i class="fa-solid fa-bars fa-fw" /> Menu
+      </button>
+      <ul class={clsx(mobileExpand && styles.mobileExpanded)}>
         {navItems.map((node) => (
           <NavNode node={node} />
         ))}
