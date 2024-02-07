0d18a6acad89b389b2d59accba6b9f7308f82404
Ben Saufley <bensaufley+git@gmail.com>
2024-02-07 09:31:01 -0500

    Prod people to auto-renew without quietly defaulting to on

--- app/javascript/users/memberships.js
+++ app/javascript/users/memberships.js
@@ -38,16 +38,9 @@
           $('#membership_stripe_card_token').val(response.id).closest('form')[0]
         ).submit();
       } else {
-        $('#stripe_card_number')
-          .closest('form')
-          .find(':submit')
-          .prop('disabled', false);
+        $('#stripe_card_number').closest('form').find(':submit').prop('disabled', false);
         $('.field_with_errors [id^=stripe]').each(function () {
-          $(this)
-            .closest('.field_with_errors')
-            .removeClass('field_with_errors')
-            .find('small.error')
-            .remove();
+          $(this).closest('.field_with_errors').removeClass('field_with_errors').find('small.error').remove();
         });
         $(stripeParams[response.error.param])
           .after('<small class="error">' + response.error.message + '</small>')
@@ -61,12 +54,7 @@
     stripePublicKey = $('meta[name="stripe-public-key"]').attr('content');
     $('#show-credit-card-info').on('click', function (e) {
       e.preventDefault();
-      $('#credit-card-info')
-        .removeClass('hide')
-        .hide()
-        .slideDown(150)
-        .find(':disabled')
-        .prop('disabled', false);
+      $('#credit-card-info').removeClass('hide').hide().slideDown(150).find(':disabled').prop('disabled', false);
       $(this).fadeOut(150, function () {
         $(this).remove();
         subscription.stripeInit();
@@ -75,5 +63,17 @@
     if (stripePublicKey && $('#credit-card-info').is(':visible')) {
       subscription.stripeInit();
     }
+
+    $('#new-membership').on('submit', function (e) {
+      const autoRenewCheckbox = /** @type {HTMLInputElement | undefined} */ (
+        $(this).find('[type=checkbox][name*=subscribe]').get(0)
+      );
+      if (!autoRenewCheckbox || autoRenewCheckbox.checked) return;
+
+      if (confirm('Are you sure you wish to continue without auto-renewal enabled?')) return;
+
+      e.preventDefault();
+      return false;
+    });
   });
-}.call(this, jQuery));
+}).call(this, jQuery);
